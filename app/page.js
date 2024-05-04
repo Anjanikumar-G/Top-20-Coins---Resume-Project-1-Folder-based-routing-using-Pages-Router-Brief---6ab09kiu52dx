3c7ed5eab51fa8e35524b7328582e47813366405
Anjanikumar Guruvali <108091230+Anjanikumar-G@users.noreply.github.com>
2024-05-04 12:34:37 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,13 +4,24 @@ import CoinCard from './components/CoinCard';
 
 function Home() {
     const [coins, setCoins] = useState([]);
+    
+    const fetchCoins = async () => {
+        const res = await fetch('https://api.coinlore.net/api/tickers/')
+        const data = await res.json()
+        const firstTwentyCoins = data.data.slice(0, 20)
+        setCoins(firstTwentyCoins)
+    }
+
+    useEffect(() => {
+        fetchCoins()
+    }, [])
 
     return (
         <div className='home'>
             <h1>Top 20 Cryptos</h1>
             <div className='coins-container'>
                 {coins.map((coin) => (
-                    <CoinCard />
+                    <CoinCard key={coin.id} coin={coin}/>
                 ))}
             </div>
         </div>
